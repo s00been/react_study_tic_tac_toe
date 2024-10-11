abeed8c2c9da1c335ee82f59acfc574033c9777a
s00been <s00been1313@gmail.com>
2024-10-11 16:27:47 +0900

    state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,17 +6,19 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     const [GameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             // 메모리에 저장되는 새로운 배열 객체로서 기존 배열 요소를 자식요소로서 저장하고 있음.
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
             // 이 상태를 변경 불가능한 방식으로 업데이트하는 것
         });
+
+        onSelectSquare();
     }
 
     return (
