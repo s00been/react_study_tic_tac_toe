30ddec734aa8d35e248e48f9d2509d85b8bcfefb
s00been <s00been1313@gmail.com>
2024-10-11 15:46:17 +0900

    game board setting 2

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,30 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [GameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            // 메모리에 저장되는 새로운 배열 객체로서 기존 배열 요소를 자식요소로서 저장하고 있음.
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+            // 이 상태를 변경 불가능한 방식으로 업데이트하는 것
+        });
+    }
+
     return (
         <ol id="game-board">
             {
-                initialGameBoard.map((row, rowIndex) => 
+                GameBoard.map((row, rowIndex) => 
                 <li key={rowIndex}>
                     <ol>
                         {
-                            row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)
+                            row.map((playerSymbol, colIndex) => 
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                            )
                         }
                     </ol>
                 </li>)
