71edb7b8d8063273df86cf959de643a8b29196e4
s00been <s00been1313@gmail.com>
2024-10-11 17:10:25 +0900

    winning setting

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combination.js";
+
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
