4d9b5f159fe7b1334061613afa2abd8b187eebf0
s00been <s00been1313@gmail.com>
2024-10-11 16:54:59 +0900

    game log setting

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,20 +6,29 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const [GameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+    let GameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            // 메모리에 저장되는 새로운 배열 객체로서 기존 배열 요소를 자식요소로서 저장하고 있음.
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-            // 이 상태를 변경 불가능한 방식으로 업데이트하는 것
-        });
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
 
-        onSelectSquare();
+        GameBoard[row][col] = player;
     }
+
+    // const [GameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         // 메모리에 저장되는 새로운 배열 객체로서 기존 배열 요소를 자식요소로서 저장하고 있음.
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //         // 이 상태를 변경 불가능한 방식으로 업데이트하는 것
+    //     });
+
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id="game-board">
@@ -30,7 +39,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
                         {
                             row.map((playerSymbol, colIndex) => 
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                             )
                         }
